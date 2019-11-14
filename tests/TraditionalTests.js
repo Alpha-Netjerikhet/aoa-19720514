f9125c5b9c9d69586e517e684533fb648cad1609
BUDAPEST\Ashraf_Abdin <ashraf_abdin@epam.com>
2019-11-14 12:02:02 +0100

    Test for v2

--- tests/TraditionalTests.js
+++ tests/TraditionalTests.js
@@ -1,31 +1,38 @@
-describe('Version 1 hackathon app', () => {
+describe('Version 2 hackathon app', () => {
+    let logo;
+    let headerEl;
+    let formGroup;
+    let userNameField;
+    let passwordField;
+    let loginBtn;
+    let rememberMeCheckbox;
+    let rememberMeLabel;
+    let socialMediaIcons;
 
-    it('test 1', async () => {
+    it('Initialize elements', async () => {
         browser.waitForAngularEnabled(false);
-        await browser.get('https://demo.applitools.com/hackathon.html');
-        // await browser.get('https://demo.applitools.com/hackathonV2.html');
-        let logo = element(by.css('.logo-w img'));
-        let headerEl = await element(by.className('auth-header'));
-        let formGroup = element.all(by.className('form-group'));
-        let userNameField = await element(by.id('username'));
-        let passwordField = await element(by.id('password'));
-        let loginBtn = element(by.partialButtonText('Log In'));
-        let userIcon = element(by.className('os-icon-user-male-circle'));
-        let fingerprintIcon = element(by.className('os-icon-fingerprint'));
-        let rememberMeCheckbox = element(by.className('form-check-input'));
-        let rememberMeLabel = element(by.className('form-check-label'));
-        let socialMediaIcons = element.all(by.css('form img'));
+        await browser.get('https://demo.applitools.com/hackathonV2.html');
+        logo = await element(by.css('.logo-w img'));
+        headerEl = await element(by.className('auth-header'));
+        formGroup = element.all(by.className('form-group'));
+        userNameField = await element(by.id('username'));
+        passwordField = await element(by.id('password'));
+        loginBtn = await element(by.partialButtonText('Log In'));
+        rememberMeCheckbox = await element(by.className('form-check-input'));
+        rememberMeLabel = await element(by.className('form-check-label'));
+        socialMediaIcons = element.all(by.css('form img'));
+    });
+
+    it('test ui elements', async () => {
         expect(logo.isDisplayed()).toBe(true, 'Logo image is not displayed');
-        expect(headerEl.getText()).toContain('Login Form', 'Header text is not correct');
+        expect(headerEl.getText()).toContain('Logout Form', 'Header text is not correct');
         expect(formGroup.first().element(by.tagName('label')).getText()).toEqual('Username', 'Username label is not correct');
-        expect(userIcon.isDisplayed()).toBe(true, 'User icon is NOT visible');
-        expect(userNameField.getAttribute('placeholder')).toContain('Enter your username', 'Username field placeholder is not correct');
-        expect(formGroup.last().element(by.tagName('label')).getText()).toEqual('Password', 'Password label is not correct');
-        expect(passwordField.getAttribute('placeholder')).toContain('Enter your password', 'Password field placeholder is not correct');
-        expect(fingerprintIcon.isDisplayed()).toBe(true, 'Fingerprint icon is NOT visible');
+        expect(userNameField.getAttribute('placeholder')).toContain('John Smith', 'Username field placeholder is not correct');
+        expect(formGroup.last().element(by.tagName('label')).getText()).toEqual('Pwd', 'Password label is not correct');
+        expect(passwordField.getAttribute('placeholder')).toContain('ABC$*1@', 'Password field placeholder is not correct');
         expect(loginBtn.isDisplayed()).toBe(true, 'Login button is NOT visible');
         expect(rememberMeCheckbox.isDisplayed()).toBe(true, 'Remember me checkbox is NOT visible');
         expect(rememberMeLabel.getText()).toContain('Remember Me', 'Remember me label is not correct');
-        expect(socialMediaIcons.count()).toEqual(3, 'Social media icons count is not correct');
+        expect(socialMediaIcons.count()).toEqual(2, 'Social media icons count is not correct');
     });
 });
